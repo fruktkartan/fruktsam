60815be96088625f7872834569f17f69d6fc357f
Daniel Lublin <daniel@lublin.se>
2020-08-27 15:49:23 +0200

    Move away mapping stuff for now

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"math"
-	"net/http"
 	"os"
 	"strings"
-	"time"
 
 	"database/sql"
 
@@ -21,82 +16,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Foo struct {
-	Lat  float64
-	Lon  float64
-	Type string
-}
-
-// http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
-
-// https://stackoverflow.com/a/238558
-func deg2rad(deg float64) float64 {
-	return math.Pi * deg / 180
-}
-func rad2deg(rad float64) float64 {
-	return 180 * rad / math.Pi
-}
-
-// Semi-axes of WGS-84 geoidal reference.
-const wgs84A = 6378137.0 // Major semiaxis [m]
-const wgs84B = 6356752.3 // Minor semiaxis [m]
-
-// Earth radius at a given latitude, according to the WGS-84 ellipsoid [m].
-func wgs84EarthRadius(lat float64) float64 {
-	// http://en.wikipedia.org/wiki/Earth_radius
-	var An = wgs84A * wgs84A * math.Cos(lat)
-	var Bn = wgs84B * wgs84B * math.Sin(lat)
-	var Ad = wgs84A * math.Cos(lat)
-	var Bd = wgs84B * math.Sin(lat)
-
-	return math.Sqrt((An*An + Bn*Bn) / (Ad*Ad + Bd*Bd))
-}
-
-// Bounding box surrounding the point at given coordinates, assuming local
-// approximation of Earth surface as a sphere of radius given by WGS84.
-func boundingBox(latDeg, lonDeg, halfsideKM float64) (latMin, lonMin, latMax, lonMax float64) {
-	var lat = deg2rad(latDeg)
-	var lon = deg2rad(lonDeg)
-	var halfSide = 1000 * halfsideKM
-
-	// Radius of Earth at given latitude
-	var radius = wgs84EarthRadius(lat)
-	// Radius of the parallel at given latitude
-	var pradius = radius * math.Cos(lat)
-
-	latMin = rad2deg(lat - halfSide/radius)
-	lonMin = rad2deg(lon - halfSide/pradius)
-	latMax = rad2deg(lat + halfSide/radius)
-	lonMax = rad2deg(lon + halfSide/pradius)
-
-	return
-}
-
-func getJSON(url string, trees *[]Foo) error {
-	var c = &http.Client{Timeout: 10 * time.Second}
-	r, err := c.Get(url)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-
-	dec := json.NewDecoder(r.Body)
-	// return json.NewDecoder(r.Body).Decode(target)
-	for {
-		if err := dec.Decode(&trees); err == io.EOF {
-			break
-		} else if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-type tree struct {
-	Key, Type, Desc, By, At, Lat, Lon string
-}
-
 type nullString struct {
 	sql.NullString
 }
@@ -105,6 +24,7 @@ func (ns nullString) String() string {
 	if !ns.NullString.Valid {
 		return ""
 	}
+
 	return ns.NullString.String
 }
 
@@ -236,55 +156,4 @@ func main() {
 			fmt.Printf("\n")
 		}
 	}
-
-	// //
-	// var trees []Foo
-	// err = getJSON("https://fruktkartan-api.herokuapp.com/edits", &trees)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(trees[0])
-
-	// boundingBox(trees[0].Lat, trees[0].Lon, 100)
-
-	// //
-	// t := trees[0]
-
-	// ctx := sm.NewContext()
-	// // TODO not gettin carto tiles, just attribution
-	// // tile := sm.NewTileProviderCartoLight()
-	// // ctx.SetTileProvider(tile)
-	// ctx.SetSize(200, 200)
-	// marker := sm.NewMarker(s2.LatLngFromDegrees(t.Lat, t.Lon), color.RGBA{0xff, 0, 0, 0xff}, 20.0)
-	// marker.Label = strings.TrimSpace(t.Type)
-	// marker.SetLabelColor(color.Black)
-	// ctx.AddMarker(marker)
-
-	// var overview, zoomin image.Image
-
-	// ctx.SetZoom(10)
-	// if overview, err = ctx.Render(); err != nil {
-	// 	panic(err)
-	// }
-
-	// ctx.SetZoom(17)
-	// if zoomin, err = ctx.Render(); err != nil {
-	// 	panic(err)
-	// }
-
-	// // starting position of the second image (bottom left)
-	// sp2 := image.Point{overview.Bounds().Dx(), 0}
-	// // new rectangle for the second image
-	// r2 := image.Rectangle{sp2, sp2.Add(zoomin.Bounds().Size())}
-	// // rectangle for the big image
-	// r := image.Rectangle{image.Point{0, 0}, r2.Max}
-	// rgba := image.NewRGBA(r)
-	// draw.Draw(rgba, overview.Bounds(), overview, image.Point{0, 0}, draw.Src)
-	// draw.Draw(rgba, r2, zoomin, image.Point{0, 0}, draw.Src)
-
-	// if err := gg.SavePNG("a.png", rgba); err != nil {
-	// 	panic(err)
-	// }
 }
-
-//https://fruktkartan-api.herokuapp.com/edits
