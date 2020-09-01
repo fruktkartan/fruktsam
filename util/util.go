2675d2648361ab18dfd87c2b52e145e10e11a57d
Daniel Lublin <daniel@lublin.se>
2020-09-01 11:41:23 +0200

    Avoid init()

--- util/util.go
+++ util/util.go
@@ -9,13 +9,12 @@ import (
 
 var loc *time.Location
 
-func init() {
-	var err error
-	if loc, err = time.LoadLocation("Europe/Stockholm"); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func FormatDate(t time.Time) string {
+	if loc == nil {
+		var err error
+		if loc, err = time.LoadLocation("Europe/Stockholm"); err != nil {
+			log.Fatal(err)
+		}
+	}
 	return monday.Format(t.In(loc), "2006-01-02 15.04", monday.LocaleSvSE)
 }
